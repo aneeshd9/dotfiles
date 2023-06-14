10fc196be9a8a2f3aa88e235e054fedadf98feef
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-06-14 09:18:57 -0400

    Added emacs config.

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
